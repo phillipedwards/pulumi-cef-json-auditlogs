589169e55a5639974a73d8e85985a5687e7fe9b6
phillip edwards <phillipedwards02@gmail.com>
2023-03-14 15:59:28 -0700

    working solution. need to fix tests and add readme

--- handler/handler.go
+++ handler/handler.go
@@ -12,7 +12,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	//"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/smithy-go/ptr"
 )
 
@@ -30,6 +29,12 @@ type AuditEvent struct {
 	Version        string            `json:"version"`
 }
 
+var (
+	s3Client          *s3.Client
+	destinationBucket string
+)
+
+// marshal our audit log event to JSON after conversion
 func (e AuditEvent) ToJson() (string, error) {
 	b, err := json.Marshal(e)
 	if err != nil {
@@ -39,11 +44,8 @@ func (e AuditEvent) ToJson() (string, error) {
 	return string(b), nil
 }
 
-var (
-	s3Client          *s3.Client
-	destinationBucket string
-)
-
+// new up the bits required across executions
+// if our destination bucket (JSON bucket) is not present it's fatal
 func init() {
 	region := os.Getenv("AWS_REGION")
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
@@ -54,6 +56,9 @@ func init() {
 
 	s3Client = s3.NewFromConfig(cfg)
 	destinationBucket = os.Getenv("DEST_BUCKET")
+	if destinationBucket == "" {
+		panic("DEST_BUCKET env var must not be nil")
+	}
 }
 
 func main() {
@@ -64,7 +69,8 @@ func handler(ctx context.Context, s3Event events.S3Event) (string, error) {
 
 	fmt.Println("processing s3 event notifications")
 
-	// iterate over the objects we received notifications for
+	// iterate over the objects we received notifications for this given notification
+	// it's possible to receive multiples
 	for _, record := range s3Event.Records {
 		s3Record := record.S3
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3Record.Bucket.Name, s3Record.Object.Key)
@@ -90,6 +96,27 @@ func handler(ctx context.Context, s3Event events.S3Event) (string, error) {
 
 		obj.Body.Close()
 
+		if numBytes <= 0 {
+			fmt.Printf("audit log file %s is empty. no action processed\n", s3Record.Object.Key)
+			continue
+		}
+
+		// to json for each record present and write to a file in the same name to output bucket
+		fName := getJsonFileNameFromObject(s3Record.Object.Key)
+
+		// query our destination bucket to see if an object with the above key already exists
+		_, err = s3Client.HeadObject(ctx, &s3.HeadObjectInput{
+			Bucket: ptr.String(destinationBucket),
+			Key:    &fName,
+		})
+
+		// if our key does not exist on the destination bucket, we'd expect a 404 error
+		// this should stop replay issues
+		if err == nil {
+			fmt.Printf("possibly duplicate notification %s. ignoring request", fName)
+			continue
+		}
+
 		// possible to have more than one line so split on newline
 		// each line could have multiple files
 		content := splitToArray(buf.String())
@@ -97,8 +124,6 @@ func handler(ctx context.Context, s3Event events.S3Event) (string, error) {
 		// read each line and parse string to event struct
 		events := parseEventsfromContent(content)
 
-		// to json for each record present and write to a file in the same name to output bucket
-		fName := getJsonFileNameFromObject(s3Record.Object.Key)
 		writeToBucket(ctx, events, fName)
 
 		fmt.Printf("successfully wrote %s to bucket %s", fName, destinationBucket)
@@ -106,29 +131,35 @@ func handler(ctx context.Context, s3Event events.S3Event) (string, error) {
 	return "", nil
 }
 
+// pulumi audit logs come in \n delimited files
 func splitToArray(raw string) []string {
 	return strings.Split(raw, "\n")
 }
 
+// split the filename out from the extension to be used for writing our new json file
 func getJsonFileNameFromObject(ceffName string) string {
 	split := strings.Split(ceffName, ".")
 	return fmt.Sprintf("%s.json", split[0])
 }
 
+// main parsing function which turns raw ceff strings into a struct
 func parseEventsfromContent(raw []string) []AuditEvent {
-	events := make([]AuditEvent, len(raw))
+	var events []AuditEvent
 	for _, r := range raw {
-		events = append(events, parseCefEvent(r))
+		if r != "" {
+			events = append(events, parseCefEvent(r))
+		}
 	}
 
 	return events
 }
 
-// Pulumi Service Audit Event follows:
-//  Timestamp host CEF:version|DeviceVendor|DeviceProduct|DeviceVersion|DeviceEventClassID|Name|severity|key=value
+// goal is to take a raw ceff string and convert it to our struct AuditEvent
+// we could also rip this straight into JSON, but I like strongly typed stuff
 func parseCefEvent(raw string) AuditEvent {
 	// ex Feb 14 14:53:01 api.pulumi.com CEF:0|Pulumi|Pulumi Service|1.0|User Login|User "tushar-pulumi-corp" logged into the Pulumi Console.|0|authenticationFailure=false dvchost=api.pulumi.com orgID=bbdf1c46-4a7b-497c-8b3d-0acf8a55e505 requireOrgAdmin=false requireStackAdmin=false rt=1676386381000 src=99.159.29.103 suser=tushar-pulumi-corp tokenID= tokenName= userID=b557a719-8291-4cd3-93e4-fa5405c0ce49
 
+	fmt.Printf("Input: %s\n", raw)
 	splits := strings.Fields(raw)
 
 	// first three members of our slice should be timestamp
@@ -169,6 +200,7 @@ func parseCefEvent(raw string) AuditEvent {
 	}
 }
 
+// part of the ceff payload is a key=value component which contains arbitrary data associated with the type of event
 func parseKv(raw string) map[string]string {
 	// raw should be space delimited key=value pairs
 	kv := strings.Fields(raw)
@@ -182,6 +214,7 @@ func parseKv(raw string) map[string]string {
 	return m
 }
 
+// take the array of AuditEvents, marshal it to JSON, and write it to the destination bucket
 func writeToBucket(ctx context.Context, events []AuditEvent, key string) error {
 	var sb strings.Builder
 	for _, event := range events {
